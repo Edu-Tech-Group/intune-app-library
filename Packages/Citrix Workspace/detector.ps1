23f75b5b27e41cfc45466d5c927a386aa7c7db79
Kurtroosbeek2 <kurt@edu-tech.be>
2021-06-17 08:35:05 +0200

    Citrix en Choco

---
+++ Packages/Citrix Workspace/detector.ps1
@@ -0,0 +1,3 @@
+if (Test-Path "${env:ProgramFiles(x86)}\Citrix\Citrix WorkSpace 2105") {
+    Write-Host "Found it!"
+}
---
+++ Packages/Citrix Workspace/uninstall.ps1
@@ -0,0 +1,5 @@
+﻿$localprograms = choco list --localonly
+if ($localprograms -like "*citrix-workspace*")
+{
+    choco uninstall citrix-workspace -y
+}
