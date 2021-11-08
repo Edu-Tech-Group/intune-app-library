d881fbe0489efe536264ea4cf8c4ccf8904427ad
Casper Naudts <casper@edu-tech.be>
2021-11-08 17:16:33 +0100

    updated detection adobe reader

--- Packages/Adobe Reader/detector.ps1
+++ Packages/Adobe Reader/detector.ps1
@@ -1,3 +1,3 @@
-if (Test-Path "${env:ProgramFiles(x86)}\Adobe\Acrobat Reader DC\Reader") {
+if (Test-Path "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe") {
     Write-Host "Found it!"
 }
