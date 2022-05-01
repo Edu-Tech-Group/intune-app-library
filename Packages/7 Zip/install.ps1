ebcca1120b21f386b98b94160a9a7f6a64d229f8
Kurt Roosbeek <kurt@edu-tech.be>
2022-05-01 08:49:44 +0200

    7zip V4 (Winget)

--- Packages/7 Zip/install.ps1
+++ Packages/7 Zip/install.ps1
@@ -1,18 +1,19 @@
 ﻿param($app)
 
 #Install Winget if not yet installed
-#Installing winget Dependencies"
-Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 
-#Getting newest winget Release
-$releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+	$hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$releases = Invoke-RestMethod -uri $releases_url
-$latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+	if(!$hasPackageManager)
+	{
+		$releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 
- # Installing winget
- Add-AppxPackage -Path $latestRelease.browser_download_url
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		$releases = Invoke-RestMethod -uri "$($releases_url)"
+		$latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
+	
+		Add-AppxPackage -Path $latestRelease.browser_download_url
+	}
 
 #Install the Requested App
 winget install $app
