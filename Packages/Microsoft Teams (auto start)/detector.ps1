ee24d05c991a68301b6dbfd5ac79fac07eceb493
Kurtroosbeek2 <73697441+Kurtroosbeek2@users.noreply.github.com>
2022-05-23 14:32:30 +0200

    Delete Teams

--- Packages/Microsoft Teams (auto start)/detector.ps1
+++
@@ -1,8 +0,0 @@
-if (Test-Path "${env:LOCALAPPDATA}\Microsoft\Teams\Update.exe") {
-    Write-Output "Found it!"
-    exit 0
-}
-else
-{
-    exit 1
-}
--- Packages/Microsoft Teams (auto start)/uninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$localprograms = choco list --localonly
-if ($localprograms -like "*microsoft-teams.install*")
-{
-    choco uninstall microsoft-teams.install -y
-}
