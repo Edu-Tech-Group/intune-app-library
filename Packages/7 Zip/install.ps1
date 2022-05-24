f517c04ecdefa7fd236b7906943eacea48e14c2b
Casper <casper@edu-tech.be>
2022-05-24 19:49:27 +0200

    Atempt winget fix

--- Packages/7 Zip/install.ps1
+++ Packages/7 Zip/install.ps1
@@ -2,20 +2,14 @@
 
 #Install Winget if not yet installed
 
-	$hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
+$hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
 
-	if(!$hasPackageManager)
-	{
-		$releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-
-		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-		$releases = Invoke-RestMethod -uri "$($releases_url)"
-		$latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
-	
-		Add-AppxPackage -Path $latestRelease.browser_download_url
-
-		exit 1641
-	}
+if(!$hasPackageManager)
+{
+	Add-AppxPackage -Path ./Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.Appx
+	Add-AppxPackage -Path ./Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx
+	Add-AppxPackage -Path ./Microsoft.DesktopAppInstaller_2021.1207.203.0_neutral___8wekyb3d8bbwe.AppxBundle
+}
 
 #Install the Requested App
 winget install $app --accept-source-agreements --force
