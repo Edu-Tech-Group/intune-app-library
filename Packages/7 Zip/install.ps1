7eb61b67cafb3fd2e6429d52914d220b7015fe86
Casper <casper@edu-tech.be>
2022-05-24 21:01:28 +0200

    Attempt 2

--- Packages/7 Zip/install.ps1
+++ Packages/7 Zip/install.ps1
@@ -8,7 +8,7 @@ if(!$hasPackageManager)
 {
 	Add-AppxPackage -Path ./Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.Appx
 	Add-AppxPackage -Path ./Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx
-	Add-AppxPackage -Path ./Microsoft.DesktopAppInstaller_2021.1207.203.0_neutral___8wekyb3d8bbwe.AppxBundle
+	Add-AppxPackage -Path ./Microsoft.DesktopAppInstaller_2022.127.2322.0_neutral___8wekyb3d8bbwe.Msixbundle
 }
 
 #Install the Requested App
