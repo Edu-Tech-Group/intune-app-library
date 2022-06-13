f2a43ec4e9df41512673e01c51617a6f64dfcb2b
Kurtroosbeek2 <73697441+Kurtroosbeek2@users.noreply.github.com>
2022-06-13 15:57:20 +0200

    Finished letter A (Winget & Choco)

--- Packages/Atom/install.ps1
+++
@@ -1,8 +0,0 @@
-﻿param($app)
-
-Add-AppxPackage -Path ./Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.Appx
-Add-AppxPackage -Path ./Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx
-Add-AppxPackage -Path ./Microsoft.DesktopAppInstaller_2022.127.2322.0_neutral___8wekyb3d8bbwe.Msixbundle
-
-#Install the Requested App
-winget install $app --accept-source-agreements --force
