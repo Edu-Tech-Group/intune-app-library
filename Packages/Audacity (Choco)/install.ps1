f4324008a8c3c7fd2b55fab67d69ca6d2e2a1c78
Kurtroosbeek2 <73697441+Kurtroosbeek2@users.noreply.github.com>
2022-06-16 15:50:21 +0200

    Fixed Choco install for apps with A

--- Packages/Audacity (Choco)/install.ps1
+++ Packages/Audacity (Choco)/install.ps1
@@ -1,13 +1,7 @@
 ﻿param($app)
-
 $filter = "*" + $app + "*"
-
-$testchoco = powershell choco -v
-if(-not($testchoco)){
-    Write-Output "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-
+Set-ExecutionPolicy Bypass
+./installChoco.ps1
 $localprograms = choco list --localonly
 if ($localprograms -like $filter)
 {
