d11743d8f92e60d50caa4d1b1715e76ee3a611d4
Kurtroosbeek2 <73697441+Kurtroosbeek2@users.noreply.github.com>
2022-06-17 12:57:56 +0200

    Logo updates

---
+++ Packages/Adobe Reader (32-bit) (Choco)/install.ps1
@@ -0,0 +1,36 @@
+﻿param($app)
+$filter = "*" + $app + "*"
+Set-ExecutionPolicy Bypass
+
+$Transcript_Path = "$env:TEMP\" + $app + ".log"
+Start-Transcript $Transcript_Path
+$PackageName = "chocolatey"
+
+try{
+    if(!(test-path "C:\ProgramData\chocolatey\choco.exe")){
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+
+    C:\ProgramData\chocolatey\choco.exe list -lo
+
+    choco feature enable -n=useRememberedArgumentsForUpgrades
+
+$localprograms = choco list --localonly
+if ($localprograms -like $filter)
+{
+    choco upgrade $app -y
+}
+Else
+{
+    choco install $app -y
+}
+
+Stop-Transcript 
+    
+    exit 0
+}catch{
+
+Stop-Transcript 
+
+    exit 1618
+}
